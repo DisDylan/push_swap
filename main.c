507cd159c806f78e94cf85cb9e614102637b27a2
dpoinsu <dylan.poinsu@gmail.com>
2021-05-17 14:30:10 +0200

    first push

---
+++ main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/14 16:43:47 by dpoinsu           #+#    #+#             */
+/*   Updated: 2021/05/17 10:09:15 by dpoinsu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	main(int argc, char **argv)
+{
+	// Si argc == 1 : return (0);
+	// while i < argc:
+	// 	if(!ft_isdigit(argv[i])
+	// 		return (ERROR);
+	// 	if (is_doublon(**argv, argv[i])
+	// 		return (ERROR);
+	
+	// sa = intervertit les deux premiers elements de a, ne fait rien si un seul ou aucun
+	// sb = pareil que sa mais pour pile b
+	// ss = sa && sb
+	// pa = premier element de b placé sur a. Rien si b vide
+	// pb = pareil que pa mais a -> b
+	// ra = décale tous les éléments vers le haut de a. Le premier -> dernier
+	// rb = pareil que ra mais pour b
+	// rr = ra && rb
+	// rra = ra à l'envers. Le dernier -> premier
+	// rrb = pareil que rra mais pour b
+	// rrr = rra && rrb
+	//
+	// TROUVER LE PLUS PETIT NOMBRE LE FAIRE REMONTER ET LE PASSER SUR B
+	// REPETER
+	// PASSER B SUR A
+}
